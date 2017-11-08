c1f01244142eee61a5ede0727a9033123ef5d7d1
rcatlord <henrypartridge@henrys-mbp.tht.local>
2017-11-08 12:10:43 +0000

    IMD 2015 data and processing script

---
+++ imd_2015/README.md
@@ -0,0 +1,16 @@
+## English Indices of Deprivation 2015 for Greater Manchester
+
+This dataset provides the overall IMD and domain deciles, ranks and scores for all LSOAs in Greater Manchester. The data derive from the [Department for Communities and Local Government](https://www.gov.uk/government/statistics/english-indices-of-deprivation-2015) and were processed using an R script.
+
+The Indices of Deprivation 2015 provide a set of relative measures of deprivation for Lower Super Output Areas based on seven domains of deprivation:
+ 
+- Barriers to Housing and Services
+- Crime
+- Education, Skills and Training Deprivation
+- Employment Deprivation
+- Health Deprivation and Disability
+- Income Deprivation
+- Living Environment Deprivation
+
+There are two additional indices: the Income Deprivation Affecting Children Index (IDACI) and the Income Deprivation Affecting Older People Index (IDAOPI).
+
---
+++ imd_2015/script.R
@@ -0,0 +1,21 @@
+##  Processing IMD 2015 data ##
+
+library(tidyverse) ; library(sf) ; library(stringi)
+
+# ------- load IMD 2015 data
+# source: http://opendatacommunities.org/data/societal-wellbeing/imd/indices
+df <- read_csv("http://opendatacommunities.org/downloads/cube-table?uri=http%3A%2F%2Fopendatacommunities.org%2Fdata%2Fsocietal-wellbeing%2Fimd%2Findices") %>% 
+  select(lsoa11cd = GeographyCode, measure = Measurement, value = Value, index_domain = `Indices of Deprivation`) %>% 
+  mutate(index_domain = as.factor(stri_sub(index_domain, 4)))
+
+# ------- extract LSOA codes for GM
+# source: http://geoportal.statistics.gov.uk/
+lsoa_codes <- st_read("https://opendata.arcgis.com/datasets/da831f80764346889837c72508f046fa_3.geojson") %>% 
+  filter(grepl('Bolton|Bury|Manchester|Oldham|Rochdale|Salford|Stockport|Tameside|Trafford|Wigan', lsoa11nm)) %>% 
+  pull(lsoa11cd)
+
+# ------- filter IMD 2015 data by GM LSOAs
+df <- filter(df, lsoa11cd %in% lsoa_codes)
+
+# ------- write results
+write_csv(df, "data/IMD_2015.csv")
