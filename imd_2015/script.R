da355b98d256c35939a027c7d990c10416aad94f
rcatlord <henrypartridge@henrys-mbp.tht.local>
2017-11-08 12:59:58 +0000

    Updated R script to include wide format

--- imd_2015/script.R
+++ imd_2015/script.R
@@ -4,7 +4,7 @@ library(tidyverse) ; library(sf) ; library(stringi)
 
 # ------- load IMD 2015 data
 # source: http://opendatacommunities.org/data/societal-wellbeing/imd/indices
-df <- read_csv("http://opendatacommunities.org/downloads/cube-table?uri=http%3A%2F%2Fopendatacommunities.org%2Fdata%2Fsocietal-wellbeing%2Fimd%2Findices") %>% 
+imd <- read_csv("http://opendatacommunities.org/downloads/cube-table?uri=http%3A%2F%2Fopendatacommunities.org%2Fdata%2Fsocietal-wellbeing%2Fimd%2Findices") %>% 
   select(lsoa11cd = GeographyCode, measure = Measurement, value = Value, index_domain = `Indices of Deprivation`) %>% 
   mutate(index_domain = as.factor(stri_sub(index_domain, 4)))
 
@@ -15,7 +15,12 @@ lsoa_codes <- st_read("https://opendata.arcgis.com/datasets/da831f80764346889837
   pull(lsoa11cd)
 
 # ------- filter IMD 2015 data by GM LSOAs
-df <- filter(df, lsoa11cd %in% lsoa_codes)
+df <- filter(imd, lsoa11cd %in% lsoa_codes)
 
-# ------- write results
-write_csv(df, "data/IMD_2015.csv")
+# ------- write results (long format)
+write_csv(df, "data/IMD_2015_long.csv")
+
+# ------- write results (wide format)
+df %>% spread(measure, value) %>% 
+  rename(score = Score, rank = Rank, decile = Decile) %>% 
+  write_csv("data/IMD_2015_wide.csv")
