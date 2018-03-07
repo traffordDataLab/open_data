5fa01bb549df1fc667a8739ea5476d08c8296f07
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-03-07 16:27:09 +0000

    Blue plaques data
    Derives from openplaques.org

---
+++ blue_plaques/README.md
@@ -0,0 +1,62 @@
+<table>
+<colgroup>
+<col style="text-align:left;"/>
+<col style="text-align:left;"/>
+</colgroup>
+
+<tbody>
+<tr>
+	<td style="text-align:left;"><strong>Dataset name</strong></td>
+	<td style="text-align:left;">Blue plaques</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Dataset description</strong></td>
+	<td style="text-align:left;">Inscriptions on and locations of blue plaques in Trafford</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Source</strong></td>
+	<td style="text-align:left;">Various</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Publisher</strong></td>
+	<td style="text-align:left;">Open Plaques</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Publisher URL</strong></td>
+	<td style="text-align:left;"><a href="http://openplaques.org/">http://openplaques.org/</a></td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Geography</strong></td>
+	<td style="text-align:left;">Local authority</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Geographic coverage</strong></td>
+	<td style="text-align:left;">Trafford</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Temporal coverage</strong></td>
+	<td style="text-align:left;">8 November 2017</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Update frequency</strong></td>
+	<td style="text-align:left;">Ongoing</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Licence</strong></td>
+	<td style="text-align:left;"><a href="https://opendatacommons.org/licenses/pddl/summary/">
+Public Domain Dedication and License 1.0</a></td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Format</strong></td>
+	<td style="text-align:left;">CSV; GeoJSON</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Openness rating</strong></td>
+	<td style="text-align:left;">&#9733&#9733&#9733&#9734&#9734&nbsp; Structured data in open format (e.g. CSV)</td>
+</tr>
+<tr>
+	<td style="text-align:left;"><strong>Last updated</strong></td>
+	<td style="text-align:left;">March 2018</td>
+</tr>
+</tbody>
+</table>
---
+++ blue_plaques/pre-processing.R
@@ -0,0 +1,48 @@
+## Blue plaques ##
+
+# Source: Open Plaques
+# Publisher URL: http://openplaques.org/
+# Licence: Public Domain Dedication and License 1.0
+
+# load libraries---------------------------
+library(tidyverse) ; library(sf)
+
+# load data ---------------------------
+plaques <- read_csv("https://s3.eu-west-2.amazonaws.com/openplaques/open-plaques-United-Kingdom-2017-11-08.csv") %>% 
+  filter(!is.na(latitude)) %>% 
+  st_as_sf(crs = 4326, coords = c("longitude", "latitude"))
+
+trafford <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/local_authority/2016/trafford_local_authority_full_resolution.geojson") %>% 
+  select(area_code, area_name)
+
+# intersect data ---------------------------
+trafford_plaques <- st_intersection(plaques, trafford)
+
+# intersect data ---------------------------
+trafford_plaques <- trafford_plaques %>% 
+  select(id, title, inscription, erected, number_of_subjects,
+         lead_subject_name, lead_subject_born_in, lead_subject_died_in, lead_subject_type,
+         lead_subject_wikipedia, main_photo, address, area_name, area_code)
+
+# check data  ---------------------------
+library(leaflet)
+popup = paste0("<a href='",  trafford_plaques$main_photo, "' target='_blank'><img style = 'width: 150px;' src = ", trafford_plaques$main_photo, ">",
+                       "</a><br/><small>", trafford_plaques$title, "</small>")
+
+
+leaflet(data = trafford_plaques) %>% 
+  setView(-2.35533522781156, 53.419025498197, zoom = 12) %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>% 
+  addPolygons(data = trafford, fillColor = "#757575", weight = 1.5, dashArray = "3", color = "#212121", fillOpacity = 0.3) %>%
+  addMarkers(label = ~as.character(title), popup = popup, options = markerOptions(riseOnHover = TRUE, opacity = 0.75)) %>% 
+  addControl("<strong>Blue Plaques in Trafford</strong><br /><em>Source: Open Plaques</em>",
+             position = 'topright')
+
+# write data ---------------------------
+st_write(trafford_plaques, "blue_plaques_trafford.geojson", driver = "GeoJSON")
+trafford_plaques %>%
+  cbind(., st_coordinates(.)) %>%
+  rename(long = X, lat = Y) %>% 
+  st_set_geometry(NULL) %>%
+  write_csv("blue_plaques_trafford.csv")
+
