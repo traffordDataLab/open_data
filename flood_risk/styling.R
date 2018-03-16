a881206f092c418306f017fb6ea187cd31b157f7
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-03-16 17:06:48 +0000

    Added styling to flood_risk GeoJSON

--- flood_risk/styling.R
+++ flood_risk/styling.R
@@ -1,32 +1,23 @@
 ## Styling features ##
 
 # load packages ---------------------------
-library(tidyverse); library(sf) ; library(geojsonio) ; library(RColorBrewer)
+library(tidyverse); library(sf)
 
 # read data ---------------------------
-geojson <- st_read("http://trafforddatalab.io/open_data/flood_risk/trafford_flood_risk.geojson")
-
+geojson <- st_read("https://www.traffordDataLab.io/open_data/flood_risk/trafford_flood_risk.geojson")
+levels(geojson$PROB_4BAND)
 # apply styles ---------------------------
-geojson_styles <- geojson_style(geojson, var = 'AQMA_Ref',
-                                stroke = brewer.pal(length(unique(geojson$)), "Blues"),
-                                stroke_width = 3,
-                                stroke_opacity = 1,
-                                fill = brewer.pal(length(unique(geojson$)), "Blues"),
-                                fill_opacity = 0.8) %>% 
-  rename(`stroke-width` = stroke.width,
-         `stroke-opacity` = stroke.opacity,
-         `fill-opacity` = fill.opacity)
-
-# check results ---------------------------
-leaflet() %>% 
-  addProviderTiles(providers$CartoDB.Positron) %>% 
-  setView(-2.35533522781156, 53.419025498197, zoom = 12) %>% 
-  addPolygons(data = geojson_styles, 
-              color = ~stroke, 
-              weight = ~`stroke-width`, 
-              opacity = ~`stroke-opacity`,
-              fillColor = ~fill,
-              fillOpacity = ~`fill-opacity`)
+geojson_styles <- geojson %>% 
+  mutate(stroke = 
+           case_when(
+             PROB_4BAND == "High" ~ "#eff3ff",
+             PROB_4BAND == "Medium" ~ "#6baed6",
+             PROB_4BAND == "Low" ~ "#bdd7e7",
+             PROB_4BAND == "Very Low" ~ "#e78ac3"),
+         `stroke-width` = 3,
+         `stroke-opacity` = 1,
+         fill = stroke,
+         `fill-opacity` = 0.8)
 
 # write data ---------------------------
 st_write(geojson_styles, "trafford_flood_risk_styled.geojson")
