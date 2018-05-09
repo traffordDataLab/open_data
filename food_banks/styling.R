307db9702e5b9ab3906ab2119a6453382d09c4a8
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2018-05-09 09:48:13 +0100

    Food banks in GM

---
+++ food_banks/styling.R
@@ -0,0 +1,17 @@
+## Styling features ##
+
+# load packages ---------------------------
+library(tidyverse); library(sf) ; library(geojsonio)
+
+# read data ---------------------------
+geojson <- st_read("http://trafforddatalab.io/open_data/food_banks/GM_food_banks.geojson")
+
+# apply styles ---------------------------
+geojson_styles <- geojson_style(geojson,
+                                color = "#fc6721",
+                                size = "medium") %>%   
+  rename(`marker-color` = marker.color,
+         `marker-size` = marker.size)
+
+# write data ---------------------------
+st_write(geojson_styles, "GM_food_banks_styled.geojson")
