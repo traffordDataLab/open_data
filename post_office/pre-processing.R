49cdbbee8e5a62acbe040080a3aa8f5bfca71edc
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-05-21 13:01:49 +0100

    Added Post Office locations

---
+++ post_office/pre-processing.R
@@ -0,0 +1,45 @@
+## Post Offices ##
+
+# Source: Ordnance Survey (OS OpenMap – Local)
+# Publisher URL: https://www.ordnancesurvey.co.uk/business-and-government/products/os-open-map-local.html
+# Licence: Open Government Licence 3.0
+
+# load libraries---------------------------
+library(tidyverse) ; library(sf)
+
+# load data ---------------------------
+sd <- st_read("OS OpenMap Local (ESRI Shape File) SD/data/SD_ImportantBuilding.shp") %>% 
+  st_as_sf(crs = 27700, coords = c("Easting", "Northing")) %>% 
+  st_transform(4326)
+se <- st_read("OS OpenMap Local (ESRI Shape File) SE/data/SE_ImportantBuilding.shp") %>% 
+  st_as_sf(crs = 27700, coords = c("Easting", "Northing")) %>% 
+  st_transform(4326)
+sj <- st_read("OS OpenMap Local (ESRI Shape File) SJ/data/SJ_ImportantBuilding.shp") %>% 
+  st_as_sf(crs = 27700, coords = c("Easting", "Northing")) %>% 
+  st_transform(4326)
+sk <- st_read("OS OpenMap Local (ESRI Shape File) SK/data/SK_ImportantBuilding.shp") %>% 
+  st_as_sf(crs = 27700, coords = c("Easting", "Northing")) %>% 
+  st_transform(4326)
+polygons <- rbind(sd, se, sj, sk)
+
+gm <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/local_authority/2016/gm_local_authority_full_resolution.geojson") %>% 
+  st_set_crs(4326) %>% 
+  select(-lat, -lon)
+trafford <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/local_authority/2016/trafford_local_authority_full_resolution.geojson") %>% 
+  st_set_crs(4326) %>% 
+  select(-lat, -lon)
+
+# filter data ---------------------------
+sites <- filter(polygons, CLASSIFICA == "Post Office")
+
+# intersect data ---------------------------
+sf_gm <- st_intersection(sites, gm)
+sf_trafford <- st_intersection(sites, trafford)
+
+# check results  ---------------------------
+plot(st_geometry(gm))
+plot(st_geometry(sf_gm), add = T)
+
+# write geospatial data ---------------------------
+st_write(sf_gm, "gm_post_offices.geojson", driver = "GeoJSON")
+st_write(sf_trafford, "trafford_post_offices.geojson", driver = "GeoJSON")
