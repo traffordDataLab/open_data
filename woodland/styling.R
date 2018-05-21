eddb9b389dd6922e72618af72d056c37b01f5caa
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-05-21 10:14:53 +0100

    Updated styling

--- woodland/styling.R
+++ woodland/styling.R
@@ -9,10 +9,13 @@ geojson <- st_read("http://trafforddatalab.io/open_data/buildings/trafford_woodl
 # apply styles ---------------------------
 geojson_styles <- geojson_style(geojson, var = 'ID',
                                 stroke = "#659D32",
-                                stroke_width = 0.3,
-                                stroke_opacity = 1) %>% 
+                                stroke_width = 3,
+                                stroke_opacity = 1,
+                                fill = "#659D32",
+                                fill_opacity = 0.8) %>% 
   rename(`stroke-width` = stroke.width,
-         `stroke-opacity` = stroke.opacity)
+         `stroke-opacity` = stroke.opacity,
+         `fill-opacity` = fill.opacity)
 
 # check results ---------------------------
 leaflet() %>% 
@@ -21,7 +24,9 @@ leaflet() %>%
   addPolygons(data = geojson_styles, 
               color = ~stroke, 
               weight = ~`stroke-width`, 
-              opacity = ~`stroke-opacity`)
+              opacity = ~`stroke-opacity`,
+              fillColor = ~fill,
+              fillOpacity = ~`fill-opacity`)
 
 # write data ---------------------------
 st_write(geojson_styles, "trafford_woodland_styled.geojson")
