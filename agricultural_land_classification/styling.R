2ca8ad19d5cd814f5850fe03766601cd31ef8fa6
James Austin <james.austin@trafford.gov.uk>
2018-06-15 14:38:40 +0100

    Amended the styling due to duplicate colour.

--- agricultural_land_classification/styling.R
+++ agricultural_land_classification/styling.R
@@ -4,18 +4,18 @@
 library(tidyverse); library(sf)
 
 # read data ---------------------------
-geojson <- st_read("http://trafforddatalab.io/spatial_data/agricultural_land_classification/trafford_agricultural_land_classification.geojson")
+geojson <- st_read("http://trafforddatalab.io/open_data/agricultural_land_classification/trafford_agricultural_land_classification.geojson")
 
 # apply styles ---------------------------
-geojson <- geojson %>% 
+geojson_styles <- geojson %>% 
   mutate(stroke = 
            case_when(
-             ALC_GRADE == "Grade 1" ~ "#4C948E",
-             ALC_GRADE == "Grade 2" ~ "#8BCAC5",
-             ALC_GRADE == "Grade 3" ~ "#D8F0ED",
-             ALC_GRADE == "Grade 4" ~ "#4C948E",
-             ALC_GRADE == "Grade 5" ~ "#E4CA93",
-             ALC_GRADE == "Non Agricultural" ~ "#AE8552",
+             ALC_GRADE == "Grade 1" ~ "#c7e9c0",
+             ALC_GRADE == "Grade 2" ~ "#a1d99b",
+             ALC_GRADE == "Grade 3" ~ "#74c476",
+             ALC_GRADE == "Grade 4" ~ "#31a354",
+             ALC_GRADE == "Grade 5" ~ "#006d2c",
+             ALC_GRADE == "Non Agricultural" ~ "#ae8552",
              ALC_GRADE == "Urban" ~ "#828282"),
          `stroke-width` = 3,
          `stroke-opacity` = 1,
@@ -23,4 +23,4 @@ geojson <- geojson %>%
          `fill-opacity` = 0.8)
 
 # write data ---------------------------
-st_write(geojson, "trafford_agricultural_land_classification_styled.geojson")
+st_write(geojson_styles, "trafford_agricultural_land_classification_styled.geojson")
