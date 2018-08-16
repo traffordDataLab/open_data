204fba139c9b8811967f5c5f3b5d2eed9b376f75
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-08-16 14:06:05 +0100

    GM-wide with additional summary statistics

---
+++ median_property_prices/README.md
@@ -0,0 +1,66 @@
+<table>
+<tr>
+	<td>Dataset name</td>
+	<td>Median property prices</td>
+</tr>
+<tr>
+	<td>Dataset description</td>
+	<td>Median property prices calculated from property transactions recorded during 2017.</td>
+</tr>
+<tr>
+	<td>Source</td>
+	<td>Price Paid Data, Land Registry</td>
+</tr>
+<tr>
+	<td>Publisher</td>
+	<td>Land Registry</td>
+</tr>
+<tr>
+	<td>Publisher URL</td>
+	<td><a href="http://landregistry.data.gov.uk/">http://landregistry.data.gov.uk/</a></td>
+</tr>
+<tr>
+	<td>Geography</td>
+	<td>Ward, LSOA</td>
+</tr>
+<tr>
+	<td>Geographic coverage</td>
+	<td>Greater Manchester</td>
+</tr>
+<tr>
+	<td>Temporal coverage</td>
+	<td>Correct as of August 2018</td>
+</tr>
+<tr>
+	<td>Update frequency</td>
+	<td>Annually</td>
+</tr>
+<tr>
+	<td>Licence</td>
+	<td><a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence</a></td>
+</tr>
+<tr>
+	<td>Attribution</td>
+	<td></td>
+</tr>
+<tr>
+	<td>Format</td>
+	<td>CSV</td>
+</tr>
+<tr>
+	<td>Openness rating</td>
+	<td>&#9733&#9733&#9733&#9734&#9734&nbsp; Structured data in open format (e.g. CSV)</td>
+</tr>
+<tr>
+	<td>Last updated</td>
+	<td>August 2018</td>
+</tr>
+<tr>
+	<td>Notes</td>
+	<td>Only 'standard price paid' property transactions were included. Property types recorded as 'other' were excluded.</td>
+</tr>
+<tr>
+	<td>Lab visualisation</td>
+	<td></td>
+</tr>
+</table>
--- median_property_prices/pre-processing.R
+++ median_property_prices/pre-processing.R
@@ -1,10 +1,12 @@
 ## Median property prices, 2017 ##
 
-# Source: Land Registry
+# Source: Land Registry, Price Paid Data
 # Publisher URL: https://data.gov.uk/dataset/land-registry-monthly-price-paid-data
 # Licence: Open Government Licence
 
-# Notes: excludes sales where propertyType is recorded as "otherPropertyType"
+# Inclusion and exclusion criteria
+# - excludes property transactions where property type is recorded as "otherPropertyType"
+# - includes only "standardPricePaidTransaction" transactions 
 
 # load libraries ---------------------------
 library(tidyverse) ; library(SPARQL) ; library(sf) 
@@ -17,23 +19,25 @@ PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
 PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX owl:     <http://www.w3.org/2002/07/owl#>
-PREFIX lrppi:   <http://landregistry.data.gov.uk/def/ppi/>
 PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
+PREFIX lrppi:   <http://landregistry.data.gov.uk/def/ppi/>
 PREFIX lrcommon: <http://landregistry.data.gov.uk/def/common/>
                 
-SELECT ?date ?paon ?saon ?street ?town ?district ?county ?postcode ?propertytype ?amount
+SELECT ?date ?paon ?saon ?street ?town ?district ?county ?postcode ?propertytype ?transactiontype ?amount
 WHERE {
 ?transx lrppi:pricePaid ?amount ;
 lrppi:transactionDate ?date ;
 lrppi:propertyAddress ?addr ;
+lrppi:transactionCategory ?transactiontype ;
 lrppi:propertyType ?propertytype .
                 
-?addr lrcommon:district 'TRAFFORD'^^xsd:string .
+?addr lrcommon:county 'GREATER MANCHESTER'^^xsd:string .
 
 FILTER ( ?date >= '2017-01-01'^^xsd:date )
 FILTER ( ?date <= '2017-12-31'^^xsd:date )
 FILTER ( ?propertytype != lrcommon:otherPropertyType )
-                
+FILTER ( ?transactiontype != lrppi:additionalPricePaidTransaction )
+
 OPTIONAL {?addr lrcommon:paon ?paon .}
 OPTIONAL {?addr lrcommon:saon ?saon .}
 OPTIONAL {?addr lrcommon:street ?street .}
@@ -52,7 +56,7 @@ df_tidy <- df %>%
   mutate(date = as.POSIXct(date, origin = '1970-01-01'))
 
 # geocode data ---------------------------
-# source: http://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-latest-centroids
+# source: ONS Postcode Directory
 postcodes <- read_csv("https://opendata.arcgis.com/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv") %>%
   select(postcode = pcds, lat, long)
 df_geocoded <- left_join(df_tidy, postcodes, by = "postcode")
@@ -64,16 +68,42 @@ sf <- df_geocoded %>%
   st_set_crs(4326)
 
 # spatial join ---------------------------
-wards <- st_read("https://www.traffordDataLab.io/spatial_data/ward/2017/trafford_ward_full_resolution.geojson")
-sf <- st_join(sf, wards, join = st_within, left = FALSE) 
+wards <- st_read("https://www.traffordDataLab.io/spatial_data/ward/2017/gm_ward_full_resolution.geojson")
+wards_sf <- st_join(sf, wards, join = st_within, left = FALSE) 
+
+lsoa <- st_read("https://www.traffordDataLab.io/spatial_data/lsoa/2011/gm_lsoa_full_resolution.geojson")
+lsoa_sf <- st_join(sf, lsoa, join = st_within, left = FALSE) 
+
+# summary statistics ---------------------------
+wards_df <- wards_sf %>% 
+  st_set_geometry(value = NULL) %>% 
+  group_by(area_code, area_name) %>% 
+  summarise(median_price = median(amount),
+            min_price = min(amount),
+            max_price = max(amount),
+            transactions = n()) %>%
+  mutate(median_price = as.integer(median_price)) %>% 
+  arrange(median_price)
+
+lsoa_df <- lsoa_sf %>% 
+  st_set_geometry(value = NULL) %>% 
+  group_by(area_code, area_name) %>% 
+  summarise(median_price = median(amount),
+            min_price = min(amount),
+            max_price = max(amount),
+            transactions = n()) %>%
+  mutate(median_price = as.integer(median_price)) %>% 
+  arrange(median_price)
 
 # write data ---------------------------
-sf %>% 
-  st_set_geometry(value = NULL) %>% 
-  group_by(area_name) %>% 
-  summarise(median = median(amount)) %>% 
-  arrange(median) %>% 
-  write_csv("ward_median_property_prices.csv")
+wards_df %>%
+  write_csv("gm_ward_median_property_prices.csv")
 
+lsoa_df %>%
+  write_csv("gm_lsoa_median_property_prices.csv")
 
-            
+filter(wards_df, area_code %in% c(paste0("E0", 5000819:5000839))) %>%
+  write_csv("trafford_ward_median_property_prices.csv")            
+
+filter(lsoa_df, area_code %in% c(paste0("E0", 1006074:1006211))) %>% View()
+  write_csv("trafford_lsoa_median_property_prices.csv") 
