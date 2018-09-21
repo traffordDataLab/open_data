d603758193cd54a2c6890a50b9729790b468c634
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-09-21 16:03:42 +0100

    Fixed error with age grouping

--- road_casualties/2017/pre-processing.R
+++ road_casualties/2017/pre-processing.R
@@ -17,73 +17,87 @@ rm(accident, casualty)
 
 ## recode data ---------------------------
 # date and time
-casualties <- mutate(casualties, 
-                     date = dmy(OutputDate),
-                     year = year(date),
-                     month = month(date, label = TRUE),
-                     day = wday(date, label = TRUE),
-                     hour = hour(OutputTime))
+casualties$date <- dmy(casualties$OutputDate)
+casualties$month <- month(casualties$date, label = TRUE)
+casualties$day <- wday(casualties$date, label = TRUE)
+casualties$hour <- hour(casualties$OutputTime)
 
 # mode of travel
-casualties <- mutate(casualties, mode = 
-                       case_when(
-                         CasTypeCode == 0 ~ "Pedestrian",
-                         CasTypeCode == 1 ~ "Pedal Cycle",
-                         CasTypeCode %in% c(2,3,4,5,96,97) ~ "Powered 2 Wheeler",
-                         CasTypeCode == 8 ~ "Taxi",
-                         CasTypeCode == 9 ~ "Car",
-                         CasTypeCode == 11 ~ "Bus or Coach",
-                         CasTypeCode %in% c(19,20,21,98) ~ "Goods Vehicle",
-                         CasTypeCode %in% c(10,14,15,16,17,18,99) ~ "Other Vehicle"
-                       ))
+casualties <- mutate(casualties, 
+                     mode = ifelse(CasTypeCode == 0, "Pedestrian",
+                                   ifelse(CasTypeCode == 1, "Pedal Cycle",
+                                          ifelse(CasTypeCode %in% c(2, 3, 4, 5), "Powered 2 Wheeler",
+                                                 ifelse(CasTypeCode == 9, "Car",
+                                                        ifelse(CasTypeCode == 8, "Taxi",
+                                                               ifelse(CasTypeCode == 11, "Bus or Coach",
+                                                                      ifelse(CasTypeCode %in% c(19, 20, 21), "Goods Vehicle", "Other Vehicle"))))))),
+                     mode = factor(mode))
 
 # casualty severity
-casualties <- mutate(casualties, severity = 
-                       case_when(
-                         CasualtySeverity == 1 ~ "Fatal", 
-                         CasualtySeverity == 2 ~ "Serious", 
-                         CasualtySeverity == 3 ~ "Slight"))
+casualties <- mutate(casualties, severity = ifelse(CasualtySeverity == 1, "Fatal", ifelse(CasualtySeverity == 2, "Serious", "Slight")))
 
 # sex
-casualties <- mutate(casualties, sex = 
-                       case_when(
-                         Sex == 1 ~ "Male", 
-                         Sex == 2 ~ "Female", 
-                         Sex == -1 ~ "Not known"))
+casualties <- mutate(casualties, sex = ifelse(Sex == 1, "Male", ifelse(Sex == 2, "Female", "Not known")))
 
 # ageband                     
-casualties <- mutate(casualties, ageband = 
-                       cut(AgeBandOfCasualty,
-                           breaks = c(0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,120),
-                           labels = c("0-4","5-9","10-14","15-19","20-24","25-29","30-34","35-39",
-                                      "40-44","45-49","50-54","55-59","60-64","65-69","70-74","75-79",
-                                      "80-84","85-89","90+"),
-                           right = FALSE))
+casualties <- mutate(casualties, ageband = factor(AgeBandOfCasualty),
+                     ageband = fct_recode(ageband, 
+                                          "0-5" = "1",
+                                          "6-10" = "2",
+                                          "11-15" = "3",
+                                          "16-20" = "4",
+                                          "21-25" = "5",
+                                          "26-35" = "6",
+                                          "36-45" = "7",
+                                          "46-55" = "8",
+                                          "56-65" = "9",
+                                          "66-75" = "10",
+                                          "Over 75" = "11"))
+
+# light conditions
+casualties <- mutate(casualties, light = ifelse(LightingCondition == 1, "Daylight", "Dark"))
 
 # local authority 
-casualties <- mutate(casualties, 
-                     area_name = factor(LocalAuthority),
+casualties <- mutate(casualties, area_name = factor(LocalAuthority),
                      area_name = fct_recode(area_name, 
-                                            "Bolton" = "100",
-                                            "Bury" = "101",
-                                            "Manchester" = "102",
-                                            "Oldham" = "104",
-                                            "Rochdale" = "106",
-                                            "Salford" = "107",
-                                            "Stockport" = "109",
-                                            "Tameside" = "110",
-                                            "Trafford" = "112",
-                                            "Wigan" = "114"))
+                                          "Bolton" = "100",
+                                          "Bury" = "101",
+                                          "Manchester" = "102",
+                                          "Oldham" = "104",
+                                          "Rochdale" = "106",
+                                          "Salford" = "107",
+                                          "Stockport" = "109",
+                                          "Tameside" = "110",
+                                          "Trafford" = "112",
+                                          "Wigan" = "114"))
 
+# add a text variable
+casualties_desc <- function(row)
+  with(as.list(row), paste0("At ", gsub('.{3}$', '', OutputTime), " on ", format.Date(date, "%A %d %B %Y"),
+                            " a ", tolower(severity),
+                            " collision occured involving ", (NumberVehicles), " vehicle(s) and ",
+                            (CasualtyNumber), " casualtie(s)."))
+strs <- apply(casualties, 1, casualties_desc)
+names(strs) <- NULL
+casualties$text <- strs
 
 # rename variables ---------------------------
-casualties <- select(casualties, AREFNO = `Accident Index`, date, year, month, day, hour, mode, 
-                     severity, sex, ageband, area_name, easting = Easting, northing = Northing)
+casualties <- select(casualties, AREFNO = `Accident Index`,
+                     date, month, day, hour, mode, severity, light, sex, ageband, area_name, text,
+                     easting = Easting, northing = Northing)
+
+# extract coordinates ---------------------------
+coords <- st_as_sf(casualties, coords = c("easting","northing")) %>% 
+  st_set_crs(27700) %>% 
+  st_transform(4326) %>% 
+  st_coordinates()
+casualties <- cbind(casualties, coords) %>% 
+  rename(lng = X, lat = Y)
 
 # write data ---------------------------
-write_csv(filter(casualties, year == 2017), "STATS19_casualty_data_2017.csv")
+write_csv(filter(casualties, date >= "2017-01-01"), "STATS19_casualty_data_2017.csv")
 
-st_as_sf(filter(casualties, year == 2017), coords = c("easting", "northing")) %>% 
+st_as_sf(filter(casualties, date >= "2017-01-01"), coords = c("easting", "northing")) %>% 
   st_set_crs(27700) %>% 
   st_transform(4326) %>% 
   mutate(lng = map_dbl(geometry, ~st_coordinates(.x)[[1]]),
