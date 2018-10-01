03cd84635a2c12d7c4350e223e4361da6c66b319
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-10-01 15:40:16 +0100

    Dentists

---
+++ dentists/README.md
@@ -0,0 +1,67 @@
+<a href="trafford_dentists.geojson"><img src="thumbnail.png" alt="Dentists" width="600"/></a>
+<table>
+<tr>
+	<td>Dataset name</td>
+	<td>Dentists</td>
+</tr>
+<tr>
+	<td>Dataset description</td>
+	<td>Dentists in Trafford and Greater Manchester</td>
+</tr>
+<tr>
+	<td>Source</td>
+	<td>Care Quality Commission</td>
+</tr>
+<tr>
+	<td>Publisher</td>
+	<td>Care Quality Commission</td>
+</tr>
+<tr>
+	<td>Publisher URL</td>
+	<td><a href="http://www.cqc.org.uk/about-us/transparency/using-cqc-data"></a>http://www.cqc.org.uk/about-us/transparency/using-cqc-data</td>
+</tr>
+<tr>
+	<td>Geography</td>
+	<td>Point data</td>
+</tr>
+<tr>
+	<td>Geographic coverage</td>
+	<td>Trafford, Greater Manchester</td>
+</tr>
+<tr>
+	<td>Temporal coverage</td>
+	<td>Correct as of 26 September 2018</td>
+</tr>
+<tr>
+	<td>Update frequency</td>
+	<td>Weekly</td>
+</tr>
+<tr>
+	<td>Licence</td>
+	<td><a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence</a></td>
+</tr>
+<tr>
+	<td>Attribution</td>
+	<td></td>
+</tr>
+<tr>
+	<td>Format</td>
+	<td>CSV, GeoJSON</td>
+</tr>
+<tr>
+	<td>Openness rating</td>
+	<td>&#9733&#9733&#9733&#9734&#9734&nbsp; Structured data in open format (e.g. CSV)</td>
+</tr>
+<tr>
+	<td>Last updated</td>
+	<td>October 2018</td>
+</tr>
+<tr>
+	<td>Notes</td>
+	<td></td>
+</tr>
+<tr>
+	<td>Lab visualisation</td>
+	<td>View data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=dentists">Explore application</a>.</td>
+</tr>
+</table>
---
+++ dentists/pre-processing.R
@@ -0,0 +1,50 @@
+## Dentists ##
+
+# Source: Care Quality Commission
+# Publisher URL: http://www.cqc.org.uk/about-us/transparency/using-cqc-data
+# Licence: Open Government Licence
+
+# load libraries ---------------------------
+library(tidyverse) ;  library(sf)
+
+# load data ---------------------------
+raw <- read_csv("https://www.cqc.org.uk/sites/default/files/26_September_2018_CQC_directory.csv", 
+                skip = 4, col_names = TRUE) %>% setNames(tolower(names(.)))
+
+area_lookup <- data_frame(
+  area_code = paste0("E080000" , seq(1:10)),
+  area_name = c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale",
+                "Salford", "Stockport", "Tameside", "Trafford", "Wigan")
+)
+
+postcodes <- read_csv("https://www.traffordDataLab.io/spatial_data/postcodes/GM_postcodes_2018-08.csv")
+
+# tidy data ---------------------------
+df <- raw %>% 
+  filter(str_detect(`service types`, "Dentist") & 
+           `local authority` %in% area_lookup$area_name) %>%
+  select(name, type = `service types`, address, postcode, telephone = `phone number`, 
+         website = `service's website (if available)`, area_name = `local authority`) %>% 
+  left_join(., area_lookup, by = "area_name") %>% 
+  left_join(., postcodes, by = "postcode")
+
+# style GeoJSON  ---------------------------
+sf <- df %>% 
+  st_as_sf(coords = c("lon", "lat")) %>%
+  st_set_crs(4326) %>%
+  rename(Name = name,
+         `Service type` = type, 
+         Address = address,
+         Postcode = postcode,
+         `Telephone` = telephone,
+         Website = website,
+         `Area name` = area_name,
+         `Area code` = area_code) %>%
+  mutate(`marker-color` = "#fc6721",
+         `marker-size` = "medium")
+
+# write data  ---------------------------
+write_csv(df, "gm_dentists.csv")
+write_csv(filter(df, area_name == "Trafford"), "trafford_dentists.csv")
+st_write(sf, "gm_dentists.geojson")
+st_write(filter(sf, `Area name` == "Trafford"), "trafford_dentists.geojson")
