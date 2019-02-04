55cd5e644ae1dfcd2a611580c0976d730dff5938
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-02-04 14:19:21 +0000

    Bus stops

--- transport/bus/stops/pre-processing.R
+++ transport/bus/stops/pre-processing.R
@@ -9,22 +9,33 @@ library(tidyverse) ; library(sf)
 
 # load and tidy data ---------------------------
 # source: "http://naptan.app.dft.gov.uk/DataRequest/Naptan.ashx?format=csv&LA=180"
-unzip("data/NaPTAN180csv.zip", exdir = "data")
-file.remove("data/NaPTAN180csv.zip")
+unzip("NaPTAN180csv.zip", exdir = ".")
+file.remove("NaPTAN180csv.zip")
 
 bdy <- st_read("https://www.traffordDataLab.io/spatial_data/local_authority/2016/trafford_local_authority_full_resolution.geojson")
 
-sf <- read_csv("data/Stops.csv") %>% 
+sf <- read_csv("Stops.csv") %>% 
   select(atco_code = ATCOCode, 
          common_name = CommonName, 
          street = Street, 
          BusStopType, Status, Longitude, Latitude) %>% 
   filter(!is.na(BusStopType) & Status == "act") %>% 
   select(-BusStopType, -Status) %>% 
+  mutate(street = gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(street), perl=TRUE)) %>% 
   st_as_sf(crs = 4326, coords = c("Longitude", "Latitude")) %>% 
   st_intersection(., bdy) %>% 
-  mutate(`marker-color` = "#fc6721",
-         `marker-size` = "medium")
+  select(1:5, lon, lat)
 
 # write data
 st_write(sf, "trafford_bus_stops.geojson")
+
+# style data
+sf %>% 
+  rename(`ATCO code` = atco_code,
+         `Common name` = common_name,
+         Street = street,
+         `Area code` = area_code, 
+         `Area name` = area_name) %>% 
+  mutate(`marker-color` = "#fc6721",
+         `marker-size` = "medium") %>% 
+  st_write("trafford_bus_stops_styled.geojson")
