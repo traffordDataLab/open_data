5736be772da07dd81d532fcbbd2182698f5189e6
James Austin <james.austin@trafford.gov.uk>
2019-04-05 18:14:46 +0100

    Updated script based on new dataset and per our new styling guidlines.

--- AQMA/pre-processing.R
+++ AQMA/pre-processing.R
@@ -8,32 +8,34 @@
 library(tidyverse) ; library(sf)
 
 # load data ---------------------------
-url <- "https://uk-air.defra.gov.uk/assets/documents/uk_aqma_July2018_Final.zip"
-download.file(url, dest = "uk_aqma_July2018_Final.zip")
-unzip("uk_aqma_July2018_Final.zip")
-file.remove("uk_aqma_July2018_Final.zip")
+url <- "https://uk-air.defra.gov.uk/assets/documents/uk_aqma_January2019_Final.zip"
+download.file(url, dest = "uk_aqma_January2019_Final.zip")
+unzip("uk_aqma_January2019_Final.zip")
+file.remove("uk_aqma_January2019_Final.zip")
 
-sf <- st_read("uk_aqma_July2018_Final.shp") %>% st_transform(4326)
+sf <- st_read("uk_aqma_January2019_Final.shp") %>% st_transform(4326)
 bdy <- st_read("https://www.traffordDataLab.io/spatial_data/local_authority/2016/trafford_local_authority_full_resolution.geojson")
 
 # tidy data ---------------------------
 gm_sf <- sf %>% 
   filter(LOCAL_AUTH == "Greater Manchester")
 
-trafford_sf <- st_intersection(gm_sf, bdy)
-
-# style GeoJSON ---------------------------
+trafford_sf <- st_intersection(gm_sf, bdy) %>% # create Trafford subset
+  select(area_code, area_name, description = DESCRIPTIO)
+  
 gm_sf <- gm_sf %>% 
-  mutate(`Area code` = "E47000001") %>% 
-  select(`Area code`, `Area name` = LOCAL_AUTH, Description = DESCRIPTIO) %>% 
+  mutate(area_code = "E47000001") %>% 
+  select(area_code, area_name = LOCAL_AUTH, description = DESCRIPTIO)
+  
+# style GeoJSON ---------------------------
+gm_styled_sf <- gm_sf %>%
   mutate(stroke = "#727C81",
          `stroke-width` = 3,
          `stroke-opacity` = 1,
          fill = "#727C81",
          `fill-opacity` = 0.8)
 
-trafford_sf <- trafford_sf %>% 
-  select(`Area code` = area_code, `Area name` = area_name, Description = DESCRIPTIO) %>% 
+trafford_styled_sf <- trafford_sf %>%
   mutate(stroke = "#727C81",
          `stroke-width` = 3,
          `stroke-opacity` = 1,
@@ -42,4 +44,6 @@ trafford_sf <- trafford_sf %>%
 
 # write data ---------------------------
 st_write(gm_sf, "gm_aqma.geojson", driver = "GeoJSON")
+st_write(gm_styled_sf, "gm_aqma_styled.geojson", driver = "GeoJSON")
 st_write(trafford_sf, "trafford_aqma.geojson", driver = "GeoJSON")
+st_write(trafford_styled_sf, "trafford_aqma_styled.geojson", driver = "GeoJSON")
