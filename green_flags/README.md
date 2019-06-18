193adbd965f71b9d8036fb7d8e2091ad6174678e
James Austin <james.austin@trafford.gov.uk>
2019-06-18 11:30:30 +0100

    Minor change to create GM and Trafford-only files.

--- green_flags/README.md
+++ green_flags/README.md
@@ -53,7 +53,7 @@
 </tr>
 <tr>
 	<td>Last updated</td>
-	<td>29-05-2019</td>
+	<td>2019-06-18</td>
 </tr>
 <tr>
 	<td>Notes</td>
@@ -61,6 +61,6 @@
 </tr>
 <tr>
 	<td>Lab visualisation</td>
-	<td>View data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=dataset_name">Explore application</a></td>
+	<td>View data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=green_flags">Explore application</a></td>
 </tr>
 </table>
--- green_flags/pre-processing.R
+++ green_flags/pre-processing.R
@@ -4,11 +4,14 @@
 # Publisher URL: http://www.greenflagaward.org.uk
 # Licence: Open Government Licence v3.0
 
-library(tidyverse) ; library(httr) ; library(jsonlite) ; library(purrr) ; library(sf)
+# -------- Load required libraries
+library(tidyverse) ; library(httr) ; library(jsonlite) ; library(purrr) ; library(sf) ; library(rvest)
 
+# -------- Get the boundary of Greater Manchester
 bdy <- st_read("https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/Local_Authority_Districts_December_2018_Boundaries_UK_BGC/MapServer/0/query?where=lad18nm%20IN%20(%27Bolton%27,%27Bury%27,%27Manchester%27,%27Oldham%27,%27Rochdale%27,%27Salford%27,%27Stockport%27,%27Tameside%27,%27Trafford%27,%27Wigan%27)&outFields=lad18cd,lad18nm&outSR=4326&f=geojson") %>% 
   select(area_code = lad18cd, area_name = lad18nm)
 
+# -------- Get the list of parks that have been awarded the Green Flag and lie within the GM boundary
 r <- POST("http://www.greenflagaward.org.uk/umbraco/surface/parks/getparks/")
 sf <- content(r, as = "text", encoding = "UTF-8") %>% 
   fromJSON(flatten = TRUE) %>% 
@@ -26,6 +29,7 @@ sf <- content(r, as = "text", encoding = "UTF-8") %>%
   filter(!is.na(area_code)) %>% 
   select(name, green_flag_page, area_code, area_name) 
 
+# -------- Get the additional information, such as contact details etc. from each individual park page in turn
 url <- "http://www.greenflagaward.org.uk/park-summary/?park=%d"
 meta <- map_df(as.numeric(gsub("\\D", "", sf$green_flag_page)), function(i) {
   query <- tryCatch(read_html(sprintf(url, i)),
@@ -43,13 +47,26 @@ meta <- map_df(as.numeric(gsub("\\D", "", sf$green_flag_page)), function(i) {
   }
 })
 
+# -------- Join the meta data to the park boundary data obtained earlier
 sf_meta <- left_join(sf, meta, by = "name") %>% 
   select(name, managed, contact, telephone, email, website, area_code, area_name)
 
-st_write(sf_meta, "green_flags.geojson")
+sf_meta_trafford <- sf_meta %>%
+  filter(area_name == "Trafford")
 
+# -------- Write out the spatial files for GM & Trafford
+st_write(sf_meta, "gm_green_flags.geojson")
+st_write(sf_meta_trafford, "trafford_green_flags.geojson")
+
+# -------- Write out the CSV data files for GM & Trafford
 sf_meta %>% 
   cbind(st_coordinates(.)) %>%
   rename(lon = X, lat = Y) %>% 
   st_set_geometry(NULL) %>% 
-  write_csv("green_flags.csv")
+  write_csv("gm_green_flags.csv")
+
+sf_meta_trafford %>%
+  cbind(st_coordinates(.)) %>%
+  rename(lon = X, lat = Y) %>%
+  st_set_geometry(NULL) %>%
+  write_csv("trafford_green_flags.csv")
