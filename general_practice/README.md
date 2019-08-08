afe395a99756f13f28c75f8680e1564581a79b78
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-08-08 15:20:35 +0100

    Updated from NHS Digital

--- general_practice/README.md
+++ general_practice/README.md
@@ -1,24 +1,23 @@
-<a href="trafford_general_practices.geojson"><img src="thumbnail.png" alt="GPs" width="600"/></a>
 <table>
 <tr>
 	<td>Dataset name</td>
-	<td>General practices</td>
+	<td>GP practices</td>
 </tr>
 <tr>
 	<td>Dataset description</td>
-	<td>The location of general practices in Trafford and Greater Manchester</td>
+	<td>The location of GP practices in Trafford</td>
 </tr>
 <tr>
 	<td>Source</td>
-	<td>Care Quality Commission</td>
+	<td>NHS Digital</td>
 </tr>
 <tr>
 	<td>Publisher</td>
-	<td>Care Quality Commission</td>
+	<td>NHS Digital</td>
 </tr>
 <tr>
 	<td>Publisher URL</td>
-	<td><a href="http://www.cqc.org.uk/about-us/transparency/using-cqc-data"></a>http://www.cqc.org.uk/about-us/transparency/using-cqc-data</td>
+	<td><a href="https://digital.nhs.uk/services/organisation-data-service/data-downloads/gp-and-gp-practice-related-data"></a>https://digital.nhs.uk/services/organisation-data-service/data-downloads/gp-and-gp-practice-related-data</td>
 </tr>
 <tr>
 	<td>Geography</td>
@@ -26,7 +25,7 @@
 </tr>
 <tr>
 	<td>Geographic coverage</td>
-	<td>Trafford, Greater Manchester</td>
+	<td>Trafford</td>
 </tr>
 <tr>
 	<td>Temporal coverage</td>
@@ -34,7 +33,7 @@
 </tr>
 <tr>
 	<td>Update frequency</td>
-	<td>Weekly</td>
+	<td>Quarterly</td>
 </tr>
 <tr>
 	<td>Licence</td>
@@ -54,14 +53,14 @@
 </tr>
 <tr>
 	<td>Last updated</td>
-	<td>3 October 2018</td>
+	<td>31 May 2019</td>
 </tr>
 <tr>
 	<td>Notes</td>
-	<td>The postcodes for each location are linked to coordinates from the ONS' Postcode Directory which contains British National Grid coordinates for address-weighted unit postcode centroids.</td>
+	<td>The postcodes for GP Practice are matched with centroids derived from the latest release of the <a href="https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-latest-centroids">ONS Postcode Directory</a></td>
 </tr>
 <tr>
 	<td>Lab visualisation</td>
-	<td>View data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=general_practices">Explore application</a>.</td>
+	<td>View data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=general_practices">Explore application</a></td>
 </tr>
 </table>
--- general_practice/pre-processing.R
+++ general_practice/pre-processing.R
@@ -1,52 +1,86 @@
-## GP Practices ##
+# GP Practices in England and Wales #
 
-# Source: Care Quality Commission
-# Publisher URL: http://www.cqc.org.uk/about-us/transparency/using-cqc-data
+# Source: NHS Digital
+# Publisher URL: https://digital.nhs.uk/services/organisation-data-service/data-downloads/gp-and-gp-practice-related-data
 # Licence: Open Government Licence
 
-# load libraries ---------------------------
-library(tidyverse) ;  library(sf)
+library(tidyverse) ; library(httr) ; library(jsonlite) ; library(lubridate) ; library(stringr) ; library(sf)
 
-# load data ---------------------------
-raw <- read_csv("https://www.cqc.org.uk/sites/default/files/03_October_2018_CQC_directory.csv", 
-                skip = 4, col_names = TRUE) %>% setNames(tolower(names(.)))
+# download and unzip data
+url <- "https://files.digital.nhs.uk/assets/ods/current/epraccur.zip"
+download.file(url, dest = "epraccur.zip")
+unzip("epraccur.zip", exdir = ".")
+file.remove("epraccur.zip")
 
-area_lookup <- data_frame(
-  area_code = paste0("E080000" , seq(1:10)),
-  area_name = c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale",
-                "Salford", "Stockport", "Tameside", "Trafford", "Wigan")
-)
+# read data
+df <- read_csv("epraccur.csv", 
+               # provide variable names
+                col_names = c(
+                  "Organisation Code", "Name", "National Grouping", 
+                  "High Level Health Geography", "Address Line 1", 
+                  "Address Line 2", "Address Line 3", "Address Line 4",
+                  "Address Line 5", "Postcode", "Open Date", "Close Date", 
+                  "Status Code",  "Organisation Sub-Type Code", "Commissioner", 
+                  "Join Provider/Purchaser Date", "Left Provider/Purchaser Date", 
+                  "Contact Telephone Number", "Null", "Null", "Null", 
+                  "Amended Record Indicator", "Null", "Provider/Purchaser", 
+                  "Null", "Prescribing Setting", "Null")) %>% 
+  # recode values
+  mutate(`Status Code` = 
+           fct_recode(`Status Code`,
+                      "Active" = "A", "Closed" = "C", "Dormant" = "D",
+                      "Proposed" = "P"),
+         `Organisation Sub-Type Code` = 
+           fct_recode(`Organisation Sub-Type Code`,
+                      "Allocated to a Provider/Purchaser Organisation" = "B", 
+                      "Not allocated to a Provider/Purchaser Organisation" = "Z"),
+         `Prescribing Setting` = 
+           fct_recode(as.character(`Prescribing Setting`),
+                      "Other" = "0", "WIC Practice" = "1", "OOH Practice" = "2",
+                      "WIC + OOH Practice" = "3", "GP Practice" = "4",
+                      "Public Health Service" = "8", "Community Health Service" = "9",
+                      "Hospital Service" = "10", "Optometry Service" = "11",
+                      "Urgent & Emergency Care" = "12", "Hospice" = "13",
+                      "Care Home / Nursing Home" = "14", "Border Force" = "15",
+                      "Young Offender Institution" = "16", "Secure Training Centre" = "17",
+                      "Secure Children's Home" = "18", "Immigration Removal Centre" = "19",
+                      "Court" = "20", "Police Custody" = "21", 
+                      "Sexual Assault Referral Centre (SARC)" = "22",
+                      "Other – Justice Estate" = "24", "Prison" = "25")) %>% 
+  # convert to date format
+  mutate(`Open Date` = as_date(as.character(`Open Date`)),
+         `Close Date` = as_date(as.character(`Close Date`))) %>% 
+  # convert to proper case
+  mutate_at(c("Name", "Address Line 1", "Address Line 2", "Address Line 3", "Address Line 4"), str_to_title) %>% 
+  # concatenate addresses to a single variable
+  unite(Address, `Address Line 1`, `Address Line 2`, `Address Line 3`, 
+        `Address Line 4`, `Address Line 5`, sep = ", ", na.rm = TRUE, remove = TRUE) %>% 
+  # drop variables starting NULL
+  select(-starts_with("NULL"))
 
-postcodes <- read_csv("https://www.traffordDataLab.io/spatial_data/postcodes/GM_postcodes_2018-08.csv")
+# optional steps ---------------------------------------------------------------
 
-# tidy data ---------------------------
-df <- raw %>% 
-  filter(str_detect(`service types`, "GPs") & 
-           `local authority` %in% area_lookup$area_name) %>%
-  select(name, type = `service types`, address, postcode, telephone = `phone number`, 
-         website = `service's website (if available)`, area_name = `local authority`) %>% 
-  left_join(., area_lookup, by = "area_name") %>% 
-  left_join(., postcodes, by = "postcode")
+# retrieve postcode centroids for your CCG
+ccg = "E38000187"
+url <- paste0("https://ons-inspire.esriuk.com/arcgis/rest/services/Postcodes/ONS_Postcode_Directory_Latest_Centroids/MapServer/0/query?where=UPPER(ccg)%20like%20'%25", URLencode(toupper(ccg), reserved = TRUE), "%25'&outFields=pcds,ccg,lat,long&outSR=4326&f=json")
+postcodes <- fromJSON(content(GET(url), "text", encoding="UTF-8"))$features$attributes %>% 
+  rename(Postcode = pcds)
 
-# style GeoJSON  ---------------------------
-sf <- df %>% 
-  st_as_sf(coords = c("lon", "lat")) %>%
+# filter GP Practices by matching postcodes
+geo <- left_join(df, postcodes, by = "Postcode") %>% 
+  filter(!is.na(ccg))
+
+# remove inactive GP practices and rename variables
+results <- geo %>% 
+  filter(`Status Code` == "Active") %>% 
+  select(Name, Address, Postcode, 
+         telephone = `Contact Telephone Number`, lon = long, lat) %>% 
+  rename_all(tolower)
+
+# write results as CSV and GeoJSON
+write_csv(results, "trafford_general_practices.csv")
+
+st_as_sf(results, coords = c("lon", "lat")) %>%
   st_set_crs(4326) %>%
-  select(Name = name,
-         `Service type` = type, 
-         Address = address,
-         Postcode = postcode,
-         Telephone = telephone,
-         Website = website,
-         `Area name` = area_name,
-         `Area code` = area_code) %>%
-  mutate(`marker-color` = "#fc6721",
-         `marker-size` = "medium")
-
-# write data  ---------------------------
-write_csv(df, "gm_general_practices.csv")
-write_csv(filter(df, area_name == "Trafford"), "trafford_general_practices.csv")
-st_write(sf, "gm_general_practices.geojson")
-st_write(filter(sf, `Area name` == "Trafford"), "trafford_general_practices.geojson")
-
+  st_write("trafford_general_practices.geojson")
 
