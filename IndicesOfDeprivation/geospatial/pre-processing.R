a871c28d743db7e305d9d3f55d4d6ed0942b75e3
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-10-16 16:52:37 +0100

    Retain LSOAs in England

--- IndicesOfDeprivation/geospatial/pre-processing.R
+++ IndicesOfDeprivation/geospatial/pre-processing.R
@@ -4,12 +4,13 @@
 # Publisher URL: http://geoportal.statistics.gov.uk/
 # Licence: Open Government Licence 3.0
 
-# Please note: LSOA have been reaggragated to 2019 local authority district boundaries
+# Please note: LSOAs have been reaggragated to 2019 local authority district boundaries
 
 library(tidyverse) ; library(sf) ; library(janitor)
 
-# Retrieve super generalised 2011 LSOA boundaries (NB _2 is generalised)
-lsoa <- st_read("https://opendata.arcgis.com/datasets/da831f80764346889837c72508f046fa_3.geojson")
+# Retrieve generalised 2011 LSOA boundaries in England
+lsoa <- st_read("https://opendata.arcgis.com/datasets/da831f80764346889837c72508f046fa_2.geojson") %>% 
+  filter(str_detect(lsoa11cd, "^E"))
 
 # Create LSOA > LA lookup from 2019 English Indices of Deprivation 
 lookup <- read_csv("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/833982/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators.csv") %>% 
@@ -22,6 +23,5 @@ sf <- left_join(lsoa, lookup, by = "lsoa11cd") %>%
   select(lsoa11cd, lsoa11nm, lad19cd, lad19nm)
 
 # Write results
-st_write(sf, "lsoa_generalised.geojson")
-st_write(sf, "lsoa_super_generalised.geojson")
+st_write(sf, "lsoa.geojson")
 
