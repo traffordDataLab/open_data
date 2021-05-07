6e2b621270643ca90ab0f97de47c880ef27adbd8
James Austin <james.austin@trafford.gov.uk>
2021-05-07 17:19:50 +0100

    Update for local elections 2021-05-06.

--- elections/polling_stations/README.md
+++ elections/polling_stations/README.md
@@ -5,7 +5,7 @@
 </tr>
 <tr>
 	<td>Dataset description</td>
-	<td>Location of polling stations within Trafford and Greater Manchester correct in readiness for the Local Elections on Thursday 2<sup>nd</sup> of May</td>
+	<td>Location of polling stations within Trafford and Greater Manchester for the Local Elections on Thursday 5<sup>th</sup> of May 2021</td>
 </tr>
 <tr>
 	<td>Source</td>
@@ -13,7 +13,7 @@
 </tr>
 <tr>
 	<td>Publisher</td>
-	<td>Democracy Club</td>
+	<td>Democracy Club Community Interest Company Company No: 0946122</td>
 </tr>
 <tr>
 	<td>Publisher URL</td>
@@ -25,15 +25,15 @@
 </tr>
 <tr>
 	<td>Geographic coverage</td>
-	<td>Manchester, Salford, Stockport, Trafford</td>
+	<td>Bolton, Manchester, Oldham, Rochdale, Salford, Stockport, Tameside, Trafford, Wigan</td>
 </tr>
 <tr>
 	<td>Temporal coverage</td>
-	<td>2<sup>nd</sup> May 2019</td>
+	<td>2021-05-06</td>
 </tr>
 <tr>
 	<td>Update frequency</td>
-	<td>Unknown</td>
+	<td>Every local election year in Trafford</td>
 </tr>
 <tr>
 	<td>Licence</td>
@@ -41,7 +41,7 @@
 </tr>
 <tr>
 	<td>Attribution</td>
-	<td></td>
+	<td>Contains OS data &copy; Crown copyright and database right 2021.<br />Contains Royal Mail data &copy; Royal Mail copyright and database right 2021.<br />Contains National Statistics data &copy; Crown copyright and database right 2021.</td>
 </tr>
 <tr>
 	<td>Format</td>
@@ -53,14 +53,14 @@
 </tr>
 <tr>
 	<td>Last updated</td>
-	<td>April 2019</td>
+	<td>2021-05-07</td>
 </tr>
 <tr>
 	<td>Notes</td>
-	<td>No data is available via the API for Bolton, Bury, Oldham, Rochdale, Tameside and Wigan as of 1<sup>st</sup> April 2019</td>
+	<td>No data is available via the API for Bury.</td>
 </tr>
 <tr>
 	<td>Lab visualisation</td>
-	<td>View data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=polling_stations">Explore application</a></td>
+	<td>View Trafford's data within the Lab's <a href="https://www.trafforddatalab.io/maps/explore/index.html?dataset=polling_stations">Explore application</a></td>
 </tr>
 </table>
--- elections/polling_stations/pre-processing.R
+++ elections/polling_stations/pre-processing.R
@@ -1,23 +1,33 @@
 # Retrieve the list of polling stations within Greater Manchester from the Democracy Club API:
-# https://wheredoivote.co.uk/api/beta/pollingstations.json?council_id=E08000009
-# https://wheredoivote.co.uk/api/beta/pollingstations/geo.json?council_id=E08000009
+# https://wheredoivote.co.uk/api/beta/councils/E08000009.json
+# https://wheredoivote.co.uk/api/beta/pollingstations.json?council_id=TRF
+# Latest update for the elections on 2021-05-06
 
-# load libraries---------------------------
+# load libraries
 library(tidyverse) ; library(sf) ; library(stringr) ; library(jsonlite)
 
-# Create a vector of Greater Manchester (GM) Local Authorities (LA) by their 9-digit Government Statistical Service (GSS) code---------------------------
-gm_authorities_vector <- c("E08000001", "E08000002", "E08000003", "E08000004", "E08000005", "E08000006", "E08000007", "E08000008", "E08000009", "E08000010")
+# Create a vector of Greater Manchester (GM) Local Authorities (LA) by their 9-digit Government Statistical Service (GSS) code
+gm_la_gss_code <- c("E08000001", "E08000002", "E08000003", "E08000004", "E08000005", "E08000006", "E08000007", "E08000008", "E08000009", "E08000010")
 
-# Associate the names of the LAs with their codes---------------------------
-names(gm_authorities_vector) <- c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale", "Salford", 
-                                  "Stockport", "Tameside", "Trafford", "Wigan")
+# Associate the names of the LAs with their codes
+#names(gm_la_gss_code) <- c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale", 
+#                           "Salford", "Stockport", "Tameside", "Trafford", "Wigan")
 
-# Create the list of 10 URLs to call the API---------------------------
-api_urls <- paste0("https://wheredoivote.co.uk/api/beta/pollingstations.json?council_id=", gm_authorities_vector)
+# Create a list of 10 URLs to call the API and obtain the council ids
+gm_la_urls <- paste0("https://wheredoivote.co.uk/api/beta/councils/", gm_la_gss_code, ".json")
+
+# List the URLs - (this can be used to get the ids below)
+gm_la_urls
+
+# Create a vector of IDs for the GM LAs
+gm_la_id <- c("BOL", "BUR", "MAN", "OLD", "RCH", "SLF", "SKP", "TAM", "TRF", "WGN")
+
+# Create the list of 10 URLs to call the API to fetch the polling stations
+api_urls <- paste0("https://wheredoivote.co.uk/api/beta/pollingstations.json?council_id=", gm_la_id)
 
 # Loop through the urls vector, calling the API in turn and return a complete dataset of GM polling stations
 df_gm_stations_src <- map_df(api_urls, function(i) {
-  df_temp <- fromJSON(i, flatten = TRUE) %>% as.tibble() # create a temporary dataframe object for each url in turn
+  df_temp <- fromJSON(i, flatten = TRUE) %>% as_tibble() # create a temporary dataframe object for each url in turn
     
   df_subset <- if (nrow(df_temp) > 0) { # so long as we have data for the LA...
       bind_rows(df_temp) # ...bind it with the other data obtained
@@ -26,11 +36,10 @@ df_gm_stations_src <- map_df(api_urls, function(i) {
   return(df_subset)
 })
 
-
-# Tidy the data---------------------------
+# Tidy the data
 df_gm_stations <- df_gm_stations_src %>%  # create a new dataframe so that we don't amend the source
   # just select the variables we are interested in
-  select(station_id, address, postcode) %>%
+  select(council_api_url = council, station_id, address, postcode) %>%
   # concatenate the address and postcode as some have their postcode within the address field and others have it separate
   unite(full_address, address, postcode, remove = TRUE, sep = "\n") %>%
   # replace any commas in the address with newline characters for consistency and to ensure we get the correct station name later on
@@ -53,7 +62,8 @@ df_gm_stations <- df_gm_stations_src %>%  # create a new dataframe so that we do
          address = full_address,
          postcode = station_postcode)
 
-# Amend incorrect or missing data---------------------------
+# --------------- THIS IS SPECIFIC TO EACH REFRESH OF THE DATA AS THE STATIONS MAY HAVE CHANGED ---------------
+# Amend incorrect or missing data
 df_gm_stations <- df_gm_stations %>%
   mutate(postcode = case_when(
       postcode == "M5 3QW" ~ "M5 3LQ",   # St. Clement's Church, Salford (postcode provided is incorrect)
@@ -63,23 +73,53 @@ df_gm_stations <- df_gm_stations %>%
       TRUE ~ postcode
     )
   )
+# ----------------------------------------------------------------------------
 
 # load postcode data (Source: ONS Postcode Directory)
 postcodes <- read_csv("https://www.trafforddatalab.io/spatial_data/postcodes/gm_postcodes.csv")
 
-# Match postcodes to retrieve coordinates and area code/name---------------------------
+# Match postcodes to retrieve coordinates plus ward and LA code/name
 df_gm_stations <- left_join(df_gm_stations, postcodes, by = "postcode")
 
-# Change the order of the variables---------------------------
-df_gm_stations <- select(df_gm_stations, id, name, address, postcode, area_code, area_name, lon, lat)
+# Fill in any blank LA code and names (due to non matches to the postcode) based on the council api URL
+df_gm_stations <- df_gm_stations %>%
+    mutate(la_code = case_when(
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/BOL/" ~ "E08000001",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/BUR/" ~ "E08000002",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/MAN/" ~ "E08000003",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/OLD/" ~ "E08000004",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/RCH/" ~ "E08000005",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/SLF/" ~ "E08000006",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/SKP/" ~ "E08000007",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/TAM/" ~ "E08000008",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/TRF/" ~ "E08000009",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/WGN/" ~ "E08000010",
+    )) %>%
+    mutate(la_name = case_when(
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/BOL/" ~ "Bolton",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/BUR/" ~ "Bury",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/MAN/" ~ "Manchester",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/OLD/" ~ "Oldham",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/RCH/" ~ "Rochdale",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/SLF/" ~ "Salford",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/SKP/" ~ "Stockport",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/TAM/" ~ "Tameside",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/TRF/" ~ "Trafford",
+      council_api_url == "https://wheredoivote.co.uk/api/beta/councils/WGN/" ~ "Wigan",
+    ))
 
-# Export the data as CSV---------------------------
+# Select the variables we want in a better order
+df_gm_stations <- select(df_gm_stations, id, name, address, postcode, ward_code, ward_name, la_code, la_name, lon, lat)
+
+# Export the data as CSV
 write_csv(df_gm_stations, "gm_polling_stations.csv")
-write_csv(filter(df_gm_stations, area_name == "Trafford"), "trafford_polling_stations.csv")
+write_csv(filter(df_gm_stations, la_name == "Trafford"), "trafford_polling_stations.csv")
 
-# Convert to spatial features object---------------------------
-sf_gm_stations <- df_gm_stations %>% st_as_sf(crs = 4326, coords = c("lon", "lat"))
+# Convert to spatial features object
+sf_gm_stations <- df_gm_stations %>% 
+  drop_na() %>%
+  st_as_sf(crs = 4326, coords = c("lon", "lat"))
 
-# Export the spatial data as GeoJSON---------------------------
+# Export the spatial data as GeoJSON
 st_write(sf_gm_stations, "gm_polling_stations.geojson")
-st_write(filter(sf_gm_stations, area_name == "Trafford"), "trafford_polling_stations.geojson")
+st_write(filter(sf_gm_stations, la_name == "Trafford"), "trafford_polling_stations.geojson")
