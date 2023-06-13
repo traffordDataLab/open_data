1675a857f5c362f1919d37acbcc9329e87fbe211
James Austin <james.austin@trafford.gov.uk>
2023-06-13 15:06:47 +0100

    Updated patient registrations to the latest data available: May 2023. This file includes the new Trafford wards and localities for the first time.

--- general_practice/patient_registrations/pre-processing.R
+++ general_practice/patient_registrations/pre-processing.R
@@ -2,9 +2,15 @@
 
 # Source: https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice
 
-# Current publication: https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/may-2022
-# GP Practices: https://files.digital.nhs.uk/FA/9CB056/gp-reg-pat-prac-map.csv
-# Patients: https://files.digital.nhs.uk/91/255D0D/gp-reg-pat-prac-all.csv
+# Current publication: https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/may-2023
+# GP Practices: https://files.digital.nhs.uk/11/9809CA/gp-reg-pat-prac-map.csv
+# Patients: https://files.digital.nhs.uk/F1/3E4D66/gp-reg-pat-prac-all.csv
+
+# NOTE: Clinical Commissioning Groups (CCG) were abolished in favour of Integrated Care Boards (ICB) in 2022.
+# Trafford GPs are now part of:
+# ICB22NM: NHS Greater Manchester Integrated Care Board
+# ICB22CD: E54000057
+# ICB22CDH: QOP
 
 
 library(tidyverse)
@@ -14,15 +20,15 @@ trafford_postcodes <- read_csv("https://www.trafforddatalab.io/spatial_data/post
   select(postcode, ward_code, ward_name, msoa_code, msoa_hcl_name, locality)
 
 # Get the list of GP practices under Trafford CCG
-trafford_gp <- read_csv("https://files.digital.nhs.uk/FA/9CB056/gp-reg-pat-prac-map.csv") %>%
-  filter(CCG_NAME == "NHS Trafford CCG") %>%
+trafford_gp <- read_csv("https://files.digital.nhs.uk/11/9809CA/gp-reg-pat-prac-map.csv") %>%
+  filter(PRACTICE_POSTCODE %in% trafford_postcodes$postcode) %>%
   select(practice_code = PRACTICE_CODE,
          practice_name = PRACTICE_NAME,
          postcode = PRACTICE_POSTCODE)
 
 # Get the current number of patient registrations
-trafford_gp_patients <- read_csv("https://files.digital.nhs.uk/91/255D0D/gp-reg-pat-prac-all.csv") %>%
-  filter(ONS_CCG_CODE == "E38000187") %>%
+trafford_gp_patients <- read_csv("https://files.digital.nhs.uk/F1/3E4D66/gp-reg-pat-prac-all.csv") %>%
+  filter(POSTCODE %in% trafford_postcodes$postcode) %>%
   rename(practice_code = CODE,
          registered_patients = NUMBER_OF_PATIENTS) %>%
   left_join(trafford_gp, by = "practice_code") %>%
